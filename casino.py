2797c20c3f81dab6b19e3e3c90fa824ad7a184f5
DEEZ NUTS <hahasometimes@sssdfg.com>
2026-04-09 09:50:07 +0200

    ertjguyhtebrhjbrejhrbt

--- casino.py
+++ casino.py
@@ -2,202 +2,23 @@ import random
 import datetime
 import io
 import math
-def print_card(ajdi):
-    if ajdi.startswith("9"):
-        if ajdi.endswith("♥"):   #Yandere dev ahhhhhh
-            for i in range(7):
-                print(ascii_art[0][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[1][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[2][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[3][i])
-    elif ajdi.startswith("10"):
-        if ajdi.endswith("♥"):
-            for i in range(7):
-                print(ascii_art[4][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[5][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[6][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[7][i])
-    elif ajdi.startswith("J"):
-        if ajdi.endswith("♥"):
-            for i in range(7):
-                print(ascii_art[8][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[9][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[10][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[11][i])
-    elif ajdi.startswith("Q"):
-        if ajdi.endswith("♥"):
-            for i in range(7):
-                print(ascii_art[12][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[13][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[14][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[15][i])
-    elif ajdi.startswith("K"):
-        if ajdi.endswith("♥"):
-            for i in range(7):
-                print(ascii_art[16][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[17][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[18][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[19][i])
-    elif ajdi.startswith("A"):
-        if ajdi.endswith("♥"):
-            for i in range(7):
-                print(ascii_art[20][i])
-        elif ajdi.endswith("♦"):
-            for i in range(7):
-                print(ascii_art[21][i])
-        elif ajdi.endswith("♣"):
-            for i in range(7):
-                print(ascii_art[22][i])
-        elif ajdi.endswith("♠"):
-            for i in range(7):
-                print(ascii_art[23][i])
 def print_hand(za_hando):
-    for i in range(7):
-        for j in range(math.ceil(len(za_hando)/2)):
-                if za_hando[j].startswith("9"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[0][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[1][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[2][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[3][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("10"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[4][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[5][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[6][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[7][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("J"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[8][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[9][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[10][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[11][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("Q"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[12][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[13][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                        
-                            print(ascii_art[14][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[15][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("K"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[16][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[17][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[18][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[19][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("A"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[20][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[21][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[22][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[23][i].rstrip("\n"), end="")
-        print("")
-    for i in range(7):
-        for j in range(math.ceil(len(za_hando)/2),len(za_hando)):
-                if za_hando[j].startswith("9"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[0][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[1][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[2][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[3][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("10"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[4][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[5][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[6][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[7][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("J"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[8][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[9][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[10][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[11][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("Q"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[12][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[13][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                        
-                            print(ascii_art[14][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[15][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("K"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[16][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[17][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[18][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[19][i].rstrip("\n"), end="")
-                elif za_hando[j].startswith("A"):
-                    if za_hando[j].endswith("♥"):
-                            print(ascii_art[20][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♦"):
-                            print(ascii_art[21][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♣"):
-                            print(ascii_art[22][i].rstrip("\n"), end="")
-                    elif za_hando[j].endswith("♠"):
-                            print(ascii_art[23][i].rstrip("\n"), end="")
-        print("")
+        matrixLength = len(za_hando)
+        matrix = []
+        for i in range(math.floor(matrixLength/4) + 1):
+                for j in range(7):
+                        matrixRow = []
+                        for k in range(4):
+                                matrixRow.append("           ")
+                        matrix.append(matrixRow)
+        for i in range(len(za_hando)):
+                column=i%4
+                row=(math.floor(i/4))*7-1
+                for j in range(7):
+                        row +=1
+                        matrix[row][column] = ascii_art[default_deck.index(za_hando[i])]
+        for i in matrix:
+                print(''.join(i))
 dt = datetime.datetime.now()
 seed=dt.day*dt.month + (dt.second+15)*random.randint(7,15)*(dt.hour+16)*(dt.minute+67) + (dt.hour+18)*(dt.minute+23)*random.randint(43,56) + (dt.hour+5)*random.randint(967,1147) + (dt.second+3)*(dt.minute+9)*random.randint(87,114) + (dt.second+7)*(dt.hour+4)*random.randint(13,21)+dt.year*dt.month*dt.day*(dt.hour + 1)*(dt.minute+1)*(dt.second+1)*random.randint(1,10)
 random.seed()
